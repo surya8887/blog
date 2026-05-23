12a7fdc602aa6145e16d0d6b03d85dc87c01382f
vijay kumar <vijayabout2001@gmail.com>
2026-05-24 02:00:59 +0530

    feat: implement centralized logging utility using pino and pino-pretty

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -2,3 +2,4 @@ export * from "./apiError/index.js";
 export * from "./apiResponse/index.js";
 export * from "./asyncHandler/index.js";
 export * from "./middlewares/error.middleware.js";
+export * from "./logger/index.js";
