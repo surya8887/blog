8bf8dea8cb8824a06475b69643400c402b87dc87
vijay kumar <vijayabout2001@gmail.com>
2026-05-24 14:02:37 +0530

    feat: support authentication via accessToken cookie in auth middleware

--- packages/common/src/middlewares/auth.middleware.ts
+++ packages/common/src/middlewares/auth.middleware.ts
@@ -17,6 +17,8 @@ export const protect = asyncHandler(async (req: Request, res: Response, next: Ne
 
   if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
     token = req.headers.authorization.split(" ")[1];
+  } else if (req.cookies && req.cookies.accessToken) {
+    token = req.cookies.accessToken;
   }
 
   if (!token) {
